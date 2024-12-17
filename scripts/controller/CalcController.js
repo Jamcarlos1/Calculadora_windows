e814c2bbc572b67afe4070c06bb907cb77ccfe73
Jean Carlos <jeancarlossilva1234@gmail.com>
2024-12-17 00:39:00 -0300

    Revert "Merge remoto com conflitos resolvidos"
    This reverts commit 2ff67dedcd198eacfa76f200d3575727d1f0bdcd.

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,223 @@
+class CalcController {
+  constructor() {
+    this._lastOperator = "";
+    this._lastNumber = "";
+
+    this._operation = [];
+    this._displayCalcEl = document.querySelector("#display");
+    this._currentDate;
+    this.initialize();
+    this.initButtonsEvents();
+  }
+
+  initialize() {
+    this.setLastNumberToDisplay();
+  }
+
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach((event) => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+
+  clearAll() {
+    this._operation = [];
+    this.setLastNumberToDisplay();
+  }
+
+  clearEntry() {
+    this._operation.pop();
+    this.setLastNumberToDisplay();
+  }
+
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value;
+  }
+
+  isOperator(value) {
+    return ["+", "-", "*", "%", "/"].indexOf(value) > -1;
+  }
+
+  pushOperation(value) {
+    this._operation.push(value);
+    if (this._operation.length > 3) {
+      this.calc();
+      console.log(this._operation);
+    }
+  }
+
+  getResult() {
+    return eval(this._operation.join(""));
+  }
+  calc() {
+    let last = "";
+
+    this._lastOperator = this.getLastItem();
+
+    if (this._operation.length < 3) {
+      let firstItem = this._operation[0];
+
+      this._operation = [firstItem, this._lastOperator, this._lastNumber];
+    }
+
+    if (this._operation.length > 3) {
+      last = this._operation.pop();
+
+      this._lastNumber = this.getResult();
+    } else if (this._operation.length == 3) {
+      this._lastNumber = this.getLastItem(false);
+    }
+
+    let result = this.getResult();
+
+    if (last == "%") {
+      result /= 100;
+
+      this._operation = [result];
+    } else {
+      this._operation = [result];
+
+      if (last) this._operation.push(last);
+    }
+
+    this.setLastNumberToDisplay();
+  }
+
+  getLastItem(isOperator = true) {
+    let lastItem;
+
+    for (let i = this._operation.length - 1; i >= 0; i--) {
+      if (this.isOperator(this._operation[i]) == isOperator) {
+        lastItem = this._operation[i];
+
+        break;
+      }
+    }
+
+    if (!lastItem) {
+      lastItem = isOperator ? this._lastOperator : this._lastNumber;
+    }
+
+    return lastItem;
+  }
+  setLastNumberToDisplay() {
+    let lastNumber;
+
+    for (let i = this._operation.length - 1; i >= 0; i--) {
+      if (!this.isOperator(this._operation[i])) {
+        lastNumber = this._operation[i];
+
+        break;
+      }
+    }
+
+    if (!lastNumber) lastNumber = 0;
+
+    this.displayCalc = lastNumber;
+  }
+  addOperation(value) {
+    if (isNaN(this.getLastOperation())) {
+      if (this.isOperator(value)) {
+        this.setLastOperation(value);
+      } else if (isNaN(value)) {
+        console.log("outra coisa", value);
+      } else {
+        this.pushOperation(value);
+        this.setLastNumberToDisplay();
+      }
+    } else {
+      if (this.isOperator(value)) {
+        this.pushOperation(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
+
+        this.setLastOperation(parseInt(newValue));
+
+        this.setLastNumberToDisplay();
+      }
+    }
+  }
+
+  setError() {
+    this.displayCalc = "Error";
+  }
+  execBtn(value) {
+    switch (value) {
+      case "C":
+        this.clearAll();
+        break;
+      case "CE":
+        this.clearEntry();
+        break;
+      case "+":
+        this.addOperation("+");
+        break;
+      case "-":
+        this.addOperation("-");
+        break;
+      case "÷":
+        this.addOperation("/");
+        break;
+      case "X":
+        this.addOperation("*");
+        break;
+      case "%":
+        this.addOperation("%");
+        break;
+      case "=":
+        this.calc();
+        break;
+      case ".":
+        this.addOperation(".");
+        break;
+
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        this.addOperation(parseInt(value));
+        break;
+      default:
+        this.setError();
+        break;
+    }
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag", (e) => {
+        let textBtn = btn.textContent;
+        this.execBtn(textBtn);
+      });
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", (e) => {
+        btn.style.cursor = "pointer";
+      });
+    });
+  }
+
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;
+  }
+
+  set displayCalc(valor) {
+    this._displayCalcEl.innerHTML = valor;
+  }
+
+  get currentDate() {
+    return this._currentDate;
+  }
+
+  set currentDate(valor) {
+    this._currentDate = valor;
+  }
+}
